2c9c18a6b27e233c578a499c9b40be9006a31e13
dennissammy77 <dennissammy77@gmail.com>
2022-12-31 16:47:46 +0300

    added url for client api access

--- pages/api/control/get_industries.js
+++ pages/api/control/get_industries.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-//import Cookies from 'universal-cookie';
+//import Cookies from 'universal-cookie'; http://localhost:5000/ https://prokemia-client.herokuapp.com/
+
 
 export default async function Get_Industries() {
-    const result = await axios.get("http://localhost:5000/api/get_industries")
+    const result = await axios.get("https://prokemia-client.herokuapp.com/api/get_industries")
     return result
 }
