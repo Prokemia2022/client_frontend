ae1525d6ac5300fb3828e19357289d0f5743ca33
dennissammy77 <dennissammy77@gmail.com>
2023-01-28 13:23:05 +0300

    undedined property in valid_email_status

--- pages/distributor/dashboardMenu.js
+++ pages/distributor/dashboardMenu.js
@@ -72,7 +72,7 @@ function DashboardMenu({setCurrentValue,distributor_data}){
 					<Text>Address: {distributor_data?.address_of_company}</Text>
 				</Flex>
 			</Flex>
-			{distributor_data?.valid_email_status == false || !distributor_dat?a.valid_email_status?
+			{distributor_data?.valid_email_status == false || !distributor_data?.valid_email_status?
 				<Flex direction='column' gap='3' w='100%' bg='' p='2' borderRadius='5'>
 					<Text fontSize='28px'fontWeight='bold' color='#009393'>Verify your email.</Text>
 					<Text >Get access to all features and be an active user on our platform by verifying your email.</Text>
