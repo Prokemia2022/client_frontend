14e755e869aa89fcf34b704b92145e18acaa145a
dennissammy77 <dennissammy77@gmail.com>
2024-02-08 03:40:29 +0300

    assigning a null value to context

--- components/Providers/dashboardContext.js
+++ components/Providers/dashboardContext.js
@@ -1,10 +1,10 @@
 import { createContext, useContext } from "react";
 
-export const dashboardContext = createContext(undefined);
+export const dashboardContext = createContext('Home' | null);
 
 export function UsedashboardContext(){
     const data = useContext(dashboardContext);
-    if (data === undefined){
+    if (data === null){
         throw new Error('useDashboardContext must be used with dashboardcontext');
     }
     return data;
